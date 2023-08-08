f5d913fd810b7a350f345f9fbaf338e2ac5b367a
Freeman <freeman1992@gmail.com>
2023-08-08 17:05:11 +0200

    [Change]: Added AimSounds

--- Data/sounds/weapons/config.cpp
+++ Data/sounds/weapons/config.cpp
@@ -10,25 +10,163 @@ class CfgPatches
 	};
 };
 
+class CfgWeapons
+{
+	class Rifle_Base;
+	class Shotgun_Base;
+	class BoltActionRifle_InnerMagazine_Base;
+	class BoltActionRifle_Base;
+	class Pistol_Base;
+	class AKM_Base: Rifle_Base
+	{
+		aimSoundSet="AK";
+	};
+	class AK101_Base: Rifle_Base
+	{
+		aimSoundSet="AK";
+	};
+	class AK74_Base: Rifle_Base
+	{
+		aimSoundSet="AK";
+	};
+	class B95_Base: Rifle_Base
+	{
+		aimSoundSet="Winchester70";
+	};
+	class Colt1911_Base: Pistol_Base
+	{
+		aimSoundSet="FNX45";
+	};
+	class CZ61_Base: Rifle_Base
+	{
+		aimSoundSet="CZ61";
+	};
+	class CZ527_Base: BoltActionRifle_Base
+	{
+		aimSoundSet="CR527";
+	};
+	class CZ75_Base: Pistol_Base
+	{
+		aimSoundSet="FNX45";
+	};
+	class Deagle_Base: Pistol_Base
+	{
+		aimSoundSet="FNX45";
+	};
+	class FAL_Base: Rifle_Base
+	{
+		aimSoundSet="SVD";
+	};
+	class FNX45_Base: Pistol_Base
+	{
+		aimSoundSet="FNX45";
+	};
+	class Glock19_Base: Pistol_Base
+	{
+		aimSoundSet="FNX45";
+	};
+	class Izh18_Base: Rifle_Base
+	{
+		aimSoundSet="CR527";
+	};
+	class Izh43Shotgun_Base: Shotgun_Base
+	{
+		aimSoundSet="Saiga";
+	};
+	class M4A1_Base: Rifle_Base
+	{
+		aimSoundSet="VSS";
+	};
+	class Magnum_Base: Pistol_Base
+	{
+		aimSoundSet="FNX45";
+	};
+	class MakarovIJ70_Base: Pistol_Base
+	{
+		aimSoundSet="FNX45";
+	};
+	class Mosin9130_Base: BoltActionRifle_InnerMagazine_Base
+	{
+		aimSoundSet="Mosin";
+	};
+	class Mp133Shotgun_Base: Shotgun_Base
+	{
+		aimSoundSet="Saiga";
+	};
+	class MP5K_Base: Rifle_Base
+	{
+		aimSoundSet="VSS";
+	};
+	class Repeater_Base: Rifle_Base
+	{
+		aimSoundSet="CR527";
+	};
+	class Ruger1022_Base: Rifle_Base
+	{
+		aimSoundSet="CR527";
+	};
+	class Saiga_Base: Rifle_Base
+	{
+		aimSoundSet="Saiga";
+	};
+	class Scout_Base: BoltActionRifle_Base
+	{
+		aimSoundSet="CR527";
+	};
+	class SKS_Base: Rifle_Base
+	{
+		aimSoundSet="CR527";
+	};
+	class SVD_Base: Rifle_Base
+	{
+		aimSoundSet="SVD";
+	};
+	class UMP45_Base: Rifle_Base
+	{
+		aimSoundSet="VSS";
+	};
+	class VSS_Base: Rifle_Base
+	{
+		aimSoundSet="VSS";
+	};
+	class Winchester70_Base: BoltActionRifle_InnerMagazine_Base
+	{
+		aimSoundSet="Winchester70";
+	};
+};
+
 class CfgSoundSets
 {
+	class baseCharacter_SoundSet;
 	class SoundSet_CRDTN_AK_Base
 	{
 		volumeFactor = 0.2;
 		frequencyFactor = 1;
 		spatial = 0;
 	};
+	class aim_in_Base_SoundSet : baseCharacter_SoundSet
+	{
+		frequencyRandomizer = 1;
+		volumeRandomizer = 1;
+	};
+	class aim_out_Base_SoundSet : baseCharacter_SoundSet
+	{
+		frequencyRandomizer = 1;
+		volumeRandomizer = 1;
+	};
 };
 
 class CfgSoundShaders
 {
-
+	class baseCharacter_SoundShader;
 	class base_closeShot_SoundShader
 	{
 		volume = 1;
 		range = 3500;
 		rangeCurve = "closeShotAttenuationCurve";
 	};
+
+	#include "aim_sound_shaders.hpp"
 
 	class closeShotRifle_SoundShader : base_closeShot_SoundShader
 	{
@@ -127,33 +265,35 @@ class CfgSoundShaders
 
 	// M4
 
-	class M4_closeShot_SoundShader : closeShotRifle_SoundShader
-	{
-		samples[] = {
-			{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shoot", 1},
-			{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shoot1", 1},
-			{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shoot2", 1},
-			{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shoot3", 1},
-			{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shoot4", 1},
-			{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shoot5", 1},
-		};
-		volume = 1.0;
-	};
+	// class M4_closeShot_SoundShader : closeShotRifle_SoundShader
+	// {
+	// 	samples[] = {
+	// 		{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shoot", 1},
+	// 		{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shoot1", 1},
+	// 		{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shoot2", 1},
+	// 		{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shoot3", 1},
+	// 		{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shoot4", 1},
+	// 		{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shoot5", 1},
+	// 	};
+	// 	volume = 1.0;
+	// };
 
-	class M4_silencerCloseShot_SoundShader : base_ProfessionalSilenced_closeShot_SoundShader
-	{
-		samples[] = {{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shot_sil", 1}};
-		volume = 1.0;
-	};
+	// class M4_silencerCloseShot_SoundShader : base_ProfessionalSilenced_closeShot_SoundShader
+	// {
+	// 	samples[] = {{"CRDTN_Sounds\Data\sounds\weapons\M4A1\m4a1_shot_sil", 1}};
+	// 	volume = 1.0;
+	// };
 
-	class M4_midShot_SoundShader : midShotRifle_SoundShader
-	{
-		samples[] = {{"CRDTN_Sounds\Data\sounds\weapons\_distance_shooting_mid\scar_distant", 1}};
-		volume = 0.56234133;
-	};
-	class M4_distShot_SoundShader : distShotRifle_SoundShader
-	{
-		samples[] = {{"CRDTN_Sounds\Data\sounds\weapons\_distance_shooting_far\scar_distant", 1}};
-		volume = 1.0;
-	};
+	// class M4_midShot_SoundShader : midShotRifle_SoundShader
+	// {
+	// 	samples[] = {{"CRDTN_Sounds\Data\sounds\weapons\_distance_shooting_mid\scar_distant", 1}};
+	// 	volume = 0.56234133;
+	// };
+	// class M4_distShot_SoundShader : distShotRifle_SoundShader
+	// {
+	// 	samples[] = {{"CRDTN_Sounds\Data\sounds\weapons\_distance_shooting_far\scar_distant", 1}};
+	// 	volume = 1.0;
+	// };
+
+	// Aim sounds
 };
